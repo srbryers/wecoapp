4b557d30af9aff5b30c8fefef795a49ba75c7903
srbryers <srbryers@gmail.com>
2024-04-22 09:20:28 -0400

    Remove Shopify orders calls

--- src/app/api/shopify/order/route.ts
+++
@@ -1,8 +0,0 @@
-import { shopify } from "@/app/_utils/shopify/api"
-
-export async function GET(request: Request) {
-
-  const orders = await shopify.orders.get()
-
-  return Response.json({ message: orders })
-}
