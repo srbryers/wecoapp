ea35b60cd3f58dec41b5cbcd5f28b87934011a8a
srbryers <srbryers@gmail.com>
2024-06-07 07:45:13 -0400

    Move snowfalke connection to function

--- src/app/_utils/snowflake/snowflake.ts
+++ src/app/_utils/snowflake/snowflake.ts
@@ -2,27 +2,26 @@ import snowflake from 'snowflake-sdk'
 import crypto from 'crypto'
 import fs from 'fs'
 
-const key = fs.readFileSync(process.env['SNOWFLAKE_PRIVATE_KEY'] as string);
-const privateKeyObject = crypto.createPrivateKey({
-  key: key,
-  format: 'pem',
-  passphrase: 'frontier45'
-});
-
-var privateKey = privateKeyObject.export({
-  format: 'pem',
-  type: 'pkcs8'
-});
-
-
 const executeQuery = async (sql: string) => {
+
+  const key = fs.readFileSync(process.env['SNOWFLAKE_PRIVATE_KEY'] as string);
+  const privateKeyObject = crypto.createPrivateKey({
+    key: key,
+    format: 'pem',
+    passphrase: 'frontier45'
+  });
+
+  var privateKey = privateKeyObject.export({
+    format: 'pem',
+    type: 'pkcs8'
+  });
 
   const connection = snowflake.createConnection({
     account: process.env['SNOWFLAKE_ACCOUNT'] || '',
     username: process.env['SNOWFLAKE_USERNAME'],
     authenticator: "SNOWFLAKE_JWT",
     privateKey: privateKey as string,
-    privateKeyPass: 'frontier45',
+    privateKeyPass: process.env['SNOWFLAKE_PRIVATE_KEY_PASS'] as string,
     warehouse: 'COMPUTE_WH',
     role: 'ACCOUNTADMIN'
   });
