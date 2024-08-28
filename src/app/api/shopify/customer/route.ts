546bfcf5e68a85ef3521cb163d633730e7141a29
srbryers <srbryers@gmail.com>
2024-08-28 13:42:58 -0700

    Add support for preflight request

--- src/app/api/shopify/customer/route.ts
+++ src/app/api/shopify/customer/route.ts
@@ -4,6 +4,21 @@ export async function GET(request: Request) {
   const queryParams = new URL(request.url).searchParams
   const action = queryParams.get('action')
 
+  const resConfig: ResponseInit = {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Content-Type': 'application/json'
+    }
+  }
+
+  if (request.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    return new Response(null, resConfig)
+  }
+
   if (!action) {
     return new Response('Invalid request', { status: 400 })
   }
@@ -15,7 +30,7 @@ export async function GET(request: Request) {
         return new Response('Email is required', { status: 400 })
       }
       const publicProfile = await shopify.customers.getPublicProfile(email)
-      return new Response(JSON.stringify(publicProfile), { status: 200, headers: { 'Content-Type': 'application/json' } })
+      return new Response(JSON.stringify(publicProfile), { status: 200, ...resConfig })
     default:
       return new Response('Invalid action', { status: 400 })
   }
