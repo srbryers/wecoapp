8a9b368b2ef17dd344bf49647bebbba328e769d3
srbryers <srbryers@gmail.com>
2024-10-02 06:28:53 -0400

    Stringify webhook order update log

--- src/app/api/webhooks/shopify/order/updated/route.ts
+++ src/app/api/webhooks/shopify/order/updated/route.ts
@@ -1,5 +1,5 @@
 export async function POST(req: Request) {
   const json = await req.json()
-  console.log("request json", json)
+  console.log("request json", JSON.stringify(json))
   return Response.json({ success: true })
 }
