e769857d16b65a5c829075cd06c5681bf3be1dcd
srbryers <srbryers@gmail.com>
2024-10-02 08:01:49 -0400

    Pass hmac onto SNOMS

--- src/app/api/webhooks/shopify/order/updated/route.ts
+++ src/app/api/webhooks/shopify/order/updated/route.ts
@@ -12,6 +12,9 @@ export async function POST(req: Request) {
 
   // Validate shopify hmac
   const hmac = req.headers.get("X-Shopify-Hmac-Sha256")
+  if (!hmac) {
+    return Response.json({ success: false, error: "Shopify HMAC not set" }, { status: 401 })
+  }
   const shopifyWebhookSecret = process.env.SHOPIFY_WEBHOOK_SECRET_ORDERS_UPDATED
   if (!shopifyWebhookSecret) {
     return Response.json({ success: false, error: "Shopify webhook secret not set" }, { status: 500 })
@@ -20,15 +23,17 @@ export async function POST(req: Request) {
 
   console.log("generatedHmac", generatedHmac)
   console.log("hmac", hmac)
-  console.log("Headers", req.headers)
   console.log("Payload", JSON.stringify(payload))
-  if (hmac !== generatedHmac) {
-    return Response.json({ success: false, error: "Invalid HMAC" }, { status: 401 })
-  }
+  // Compare hmacs
+  const result = crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(generatedHmac))
+  console.log("HMAC comparison result", result)
+  // if (!result) {
+  //   return Response.json({ success: false, error: "Invalid HMAC" }, { status: 401 })
+  // }
 
   // Check if order has been updated in the last 24hrs
   if (orderLastUpdated.getTime() > (now.getTime() - (24 * 60 * 60 * 1000)) && orderFulfillmentStatus !== "fulfilled") {
-    console.log("Order has been updated in the last 24hrs and is not fulfilled and paid", JSON.stringify(order))
+    console.log("Order has been updated in the last 24hrs and is not fulfilled and paid")
     // Add/update the job in CIGO
     // const job = await cigo.jobs.create()
     //Send update to SNOMS
@@ -39,12 +44,12 @@ export async function POST(req: Request) {
         "X-Shopify-Hmac-Sha256": hmac,
         "X-Shopify-Topic": "orders/updated"
       },
-      body: JSON.stringify(order),
+      body: JSON.stringify(payload),
     }).then((res) => res.text())
     console.log("res", res)
     return Response.json({ success: true, updated: true, sentUpdateToSNOMS: true, res: res, order: order })
   } else {
-    console.log("Order has not been updated in the last 24hrs", JSON.stringify(order))
+    console.log("Order has not been updated in the last 24hrs")
     return Response.json({ success: true, updated: false, sentUpdateToSNOMS: false, res: null, order: order })
   }
 }
